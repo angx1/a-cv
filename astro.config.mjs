56c6d4430062d63282f1c3fd2f5b337f82ee57ec
angx1 <angel.rodriguez3rm@gmail.com>
2024-09-15 18:40:47 +0200

    github pages action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://angx1.github.io',
   base: 'a-cv',
-})
+});
