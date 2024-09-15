17dd8ddd6afb60d2b3208804eb66a5d4a481edf7
angx1 <angel.rodriguez3rm@gmail.com>
2024-09-15 17:13:07 +0200

    adjustments to astro.config.mjs and src/layouts/Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://angx1.github.io',
-    base: '/a-cv/'
+    site: 'https://angx1.github.io/a-cv/',
+    base: '/a-cv/',
+    
+    build: {
+        assetsPrefix: './' // Ensure the asset URLs have the correct prefix
+      },
 });
