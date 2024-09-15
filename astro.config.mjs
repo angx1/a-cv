f0a49d93252f4ef9e96f61113e9d8a19c6bb988a
angx1 <angel.rodriguez3rm@gmail.com>
2024-09-15 16:19:43 +0200

    Changes made on astro.config to fix style issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://angx1.github.io',
-    base: '/repositoryname/'
+    base: '/a-cv/'
 });
