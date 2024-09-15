478cc92b7217f9d992f5c053dc5912a1011dde5b
angx1 <angel.rodriguez3rm@gmail.com>
2024-09-15 17:38:15 +0200

    anges to fix 404 error on css files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://angx1.github.io/a-cv/',
-    base: '/a-cv/',
-    
-    build: {
-        assetsPrefix: './' // Ensure the asset URLs have the correct prefix
-      },
-});
+  site: 'https://angx1.github.io',
+  base: 'a-cv',
+})
